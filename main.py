15d1cab2bd53e6162cfc8e685582a2f2c7726fe3
Bryan Budreau <bryan.budreau@gmail.com>
2018-03-30 22:28:16 -0500

    working on if statements

--- main.py
+++ main.py
@@ -10,5 +10,23 @@ app.config['DEBUG'] = True
 def index():
     return render_template('user_form.html')
 
+@app.rout("/valdiate-form", methods=['POST'])
+def validate_form():
+    username = request.form['username']
+    password = request.form['password']
+    email = request.form['email']
+
+    username_error = ''
+    password_error = ''
+    email_error = ''
+    
+    if username > 20 or username <3:
+        username_error = "username not valid"
+        username = ''
+        
+
+
+
+
 
 app.run()
