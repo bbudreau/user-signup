dc3ceae3e3f9b225a1ddce1349e46a4a13af0a51
Bryan Budreau <bryan.budreau@gmail.com>
2018-04-08 05:51:06 -0500

    testing changes to all forms

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect, render_template
 import cgi
 import os
+import re
 
 
 app = Flask(__name__)
@@ -8,24 +9,61 @@ app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-    return render_template('user_form.html')
+    return render_template('user_form.html', title = "Sign Up")
 
-@app.route("/valdiate-form", methods=['POST'])
-def validate_form():
+@app.route("/signup", methods=['POST'])
+def signup():
     username = request.form['username']
     password = request.form['password']
+    verify = request.form['verify']
     email = request.form['email']
+   
 
     username_error = ''
     password_error = ''
+    verify_error = ''
     email_error = ''
     
-    if username > 20 or username <3:
-        username_error = "username not valid"
-        username = ''
-    if password > 20 or password <3:
-        password_error = "password is not valid"
-        password = ''
+    
+    if username == "":
+        username_error = "Please enter a valid username."
+    elif len(username) <= 3 or len(username) > 20:
+        username_error = "Username must be between 3 and 20 characters long."
+        username = ""
+    elif " " in username:
+        username_error = "Your username cannot contain any spaces."
+        username = ""
+    
+    if password == "":
+        password_error = "Please enter a valid password."
+    elif len(password) <3 or len(password) > 20:
+        password_error = "Password must be between 3 and 20 characters long."
+    elif " " in password:
+        password_error = "Your password cannot contain any spaces."
+    
+    if verify == "" or verify != password:
+        verify_error = "Passwords do not match. Please try again."
+        verify = ""
+
+    #if email != "":
+        #if not re.match(r'(^[/S]+@[/S]+\.[/S]+$)'):
+            #email_error = "Not a valid email address."
+
+    if not username_error and not password_error and not verify_error and not email_error:
+        return render_template('welcome.html', username = username)
+    else:
+        return render_template(
+        'user_form.html',
+        username = username,
+        username_error = username_error,
+        password_error = password_error,
+        verify_error = verify_error,
+        email = email,
+        email_error = email_error,
+
+        )
+
+
     
     
 
